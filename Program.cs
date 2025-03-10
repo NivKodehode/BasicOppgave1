d7ffbd22e1b0d33585ab198fbb9810f0ff772a2d
¨Niv <bn1702kodehode@gmail.com>
2025-03-10 10:22:44 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace BasicOppgave1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("What is the temperature currently?");
+
+            int temp = 0;
+            string? userInput = Console.ReadLine();
+
+            if(int.TryParse(userInput, out temp) && temp <= 12)
+            {
+                Console.WriteLine("It is cold outside");
+            }
+        else if (temp >= 25)
+        {
+            Console.WriteLine("It is hot outside");
+        }
+        else
+        {
+            Console.WriteLine("It is a nice temperature outside");
+        }
+    }
+}
