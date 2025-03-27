afc0d64454948ccf8a42954e550f9c158dd26d8d
¨Niv <bn1702kodehode@gmail.com>
2025-03-27 09:26:28 +0100

    burde ha gjort den til public

---
+++ Public/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Public;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+                Console.WriteLine("What is the temperature currently?");
+        int temp = 0;
+
+        string? userInput = Console.ReadLine();
+
+        if(int.TryParse(userInput, out temp) && temp <= 11)
+        {
+            Console.WriteLine("It is cold outside");
+
+        }
+        else if (temp >= 23)
+        {
+            Console.WriteLine("It is hot outside");
+        }
+        else
+        {
+            Console.WriteLine("It is a nice temperature outside");
+        }
+    }
+}
+
+
+
